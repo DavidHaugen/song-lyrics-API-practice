af8e1799db79c3b7cc49496aa74ea17538c459c5
David Haugen <haugen.dj@gmail.com>
2019-01-26 13:45:32 -0600

    add styling, fix functions

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 'use strict';
 /* global $ */
 
-function generateUrl(artist, title){
+function generateURI(artist, title){
   const enArtist = encodeURI(artist);
   const enTitle = encodeURI(title);
   const endUrl = `https://api.lyrics.ovh/v1/${enArtist}/${enTitle}`;
   return endUrl;
 }
 
+const errMessage =
+  '<p> Uh-oh. It looks like we couldn\'t find any results matching your search. Sorry about that. La la la la laaa</p>';
+
 
 function getLyrics(artist, title) {
-  return fetch('https://api.lyrics.ovh/v1/Coldplay/Adventure%20of%20a%20Lifetime')
-    .then(response => response.json())
-    .then(data => data.lyrics);
+  const finalUrl = generateURI(artist, title);
+  return fetch(finalUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(data => displayResults(data.lyrics))
+    .catch( () => {
+      $('#results').html(errMessage);
+    });
 }
 
 function generateHtml(data){
-  
+  return `<p>${data}</p>`;
 }
 
 function displayResults(data) {
-  $('#results').html;
+  $('#results').html(generateHtml(data));
 }
 
 function handleSearch(){
   $('.js-search-form').on('submit', function(event){
     event.preventDefault();
     const artist = $('.js-query-artist').val();
-    const title = $('js-query-title').val();
-
+    const title = $('.js-query-title').val();
     getLyrics(artist,title);
-
   });
 }
 
@@ -39,4 +49,3 @@ function watchForm() {
 }
 
 $(watchForm);
-
